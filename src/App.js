89ba98ab2f26914f1b945670f03a7f048b753db8
Sidong Yang <realwakka@gmail.com>
2019-04-19 01:52:25 +0000

    add game.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component, createContext } from 'react';
+import Game from './Game';
+
+const Context = createContext();
 
 class App extends Component {
   render() {
-    return <div> I'M READY TO USE THE BACK END APIS! :-)</div>;
+    return <Board />;
+  }
+}
+
+class Cell extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    if(this.props.game.turn === 1){
+      if(this.props.game.move(this.props.col, this.props.row) == 0) {
+	var game_tmp = this.props.game;
+        setTimeout(function() { 
+	  console.log("asdf"); 
+	  return game_tmp.play_the_game();}, 0)
+	console.log("handle click");
+      }
+    }
+    this.forceUpdate();
+  }
+
+  render() {
+    return <button onClick={this.handleClick}>
+   {this.props.game.get_state(this.props.col,this.props.row)}</button>;
+  }
+}
+
+class Row extends Component {
+  render() {
+    var cells = [];
+    for (var i = 0; i < 15; i++) {
+      cells.push(<Cell game={this.props.game} col={i} row={this.props.row}/>);
+    }
+    return (<div style={{display: 'flex'}}> {cells} </div>);
+  }
+}
+
+
+class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {game: new Game(15,15)};
+  }
+
+  render() {
+    var rows = [];
+    for (var i = 0; i < 15; i++) {
+      rows.push(<Row game={this.state.game} row={i}/>);
+    }
+    return (rows);
   }
 }
 
---
+++ src/Game.js
@@ -0,0 +1,137 @@
+var events = require('events');
+
+function check(game, x, y, front, back) {
+  let state = game.get_state(x, y);
+
+  let check_tmp = (game, fx, fy, func, state) => {
+    let count = 0;
+
+    do {
+      count++;
+      [fx, fy] = func(fx, fy);
+    } while (game.get_state(fx,fy) === state);
+    return count;
+  }
+  let total_count = check_tmp(game, x, y, back, state) + check_tmp(game, x, y, front, state);
+  console.log("total : " + total_count);
+  if (total_count === 6) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function check_finish(game, x, y) {
+  if (check(game, x, y, (x, y) => { return [x+1, y]; }, (x, y) => { return [x-1, y]; }))
+    return true;
+
+  if (check(game, x, y, (x, y) => { return [x, y+1]; }, (x, y) => { return [x, y-1]; }))
+    return true;
+
+  if (check(game, x, y, (x, y) => { return [x+1, y+1]; }, (x, y) => { return [x-1, y-1]; }))
+    return true;
+
+  if (check(game, x, y, (x, y) => { return [x+1, y-1]; }, (x, y) => { return [x-1, y+1]; }))
+    return true;
+
+  return false;
+}
+
+class Game {
+  constructor(width, height) {
+    this.board_state = [];
+    this.width = width;
+    this.height = height;
+
+    this.initialize();
+  }
+
+  initialize() {
+    this.board_state = [];
+    for (var i = 0; i < this.width * this.height; i++)
+      this.board_state.push(0);
+    this.turn = 1;
+
+    this.player1 = new WebPlayer();
+    this.player1.on_init(this.width, this.height, 1);
+
+    this.player2 = new AIPlayer();
+    this.player2.on_init(this.width, this.height, 2);
+    this.event_emitter = new events.EventEmitter();
+
+    this.event_emitter.on("play", this.play_the_game);
+  }
+
+  get_state(x, y) {
+    if (x >= 0 && x < this.width && y >= 0 && y < this.height) {
+      return this.board_state[y * this.width + x];
+    } else {
+      return -1;
+    }
+  }
+
+  move(x, y) {
+    if (this.get_state(x,y) === 0) {
+      this.board_state[y * this.width + x] = this.turn;
+
+      if (check_finish(this, x,y)) {
+	window.alert("FINISHED!!!");
+	this.initialize();
+	return this.turn + 2;
+      }
+
+      return 0;
+    } else {
+      return -1;
+    }
+  }
+
+  swap_turn() {
+    if(this.turn === 1) {
+      this.turn = 2;
+    } else {
+      this.turn = 1;
+    }
+  }
+
+  check_finish(x, y) {
+    return check_finish(this, x, y);    
+  }
+
+  play_the_game() {
+    console.log(this);
+    this.swap_turn();
+    console.log("play_the_game!");
+    if (this.turn === 1) {
+      this.player1.on_my_turn(this);
+    } else {
+      this.player2.on_my_turn(this);
+    }
+  }
+}
+
+class WebPlayer {
+  on_init(width, height, turn) {
+
+  }
+
+  on_my_turn(game) {
+
+  }
+}
+
+class AIPlayer {
+  on_init(width, height, turn) {
+    this.width = width;
+    this.height = height;
+    this.turn = turn;
+  }
+
+  on_my_turn(game) {
+    game.move(parseInt(Math.random() * this.width),parseInt(Math.random() * this.height));
+    setTimeout(function() {return game.play_the_game();}, 0);
+  }
+}
+
+
+export default Game;
